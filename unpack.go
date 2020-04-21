236553235afc5f9204c70463b0cf6247a56b22aa
Saddam H <thedevsaddam@gmail.com>
2020-04-21 19:46:42 +0600

    Add error while type mismatch

--- unpack.go
+++ unpack.go
@@ -1,18 +1,20 @@
 package unpack
 
 import (
+	"errors"
+	"fmt"
 	"reflect"
 )
 
-// Do perform a destructuring on slice values
-func Do(src interface{}, dest ...interface{}) {
+// Do perform a destructuring on slice/array values
+func Do(src interface{}, dest ...interface{}) error {
 	srcRV := reflect.ValueOf(src)
 	dstRV := reflect.ValueOf(dest)
 
 	dstLen := dstRV.Len()
 
 	if dstLen == 0 {
-		return
+		return errors.New("unpack: destination can't be empty")
 	}
 
 	if srcRV.Kind() == reflect.Slice ||
@@ -20,11 +22,18 @@ func Do(src interface{}, dest ...interface{}) {
 		for i := 0; i < srcRV.Len(); i++ {
 
 			if dstLen == 0 {
-				return
+				return nil
+			}
+
+			if dstRV.Index(i).Elem().Kind() != reflect.Ptr {
+				return errors.New("unpack: the destination is not pointers")
 			}
 
 			val := srcRV.Index(i)
 			v := dstRV.Index(i).Elem().Elem()
+			if val.Kind() != v.Kind() {
+				return fmt.Errorf("unpack: can't assign value from source type %v to destination type %v", val.Kind(), v.Kind())
+			}
 			if v.CanSet() {
 				v.Set(val)
 			}
@@ -32,4 +41,6 @@ func Do(src interface{}, dest ...interface{}) {
 			dstLen--
 		}
 	}
+
+	return nil
 }
--- unpack_test.go
+++ unpack_test.go
@@ -9,7 +9,7 @@ func TestDo(t *testing.T) {
 	t.Run("Should assign all names", func(t *testing.T) {
 		names := []string{"Tom", "Jerry", "Sparrow"}
 		var tom, jerry, sparrow string
-		Do(names, &tom, &jerry, &sparrow)
+		_ = Do(names, &tom, &jerry, &sparrow)
 		if tom != "Tom" || jerry != "Jerry" || sparrow != "Sparrow" {
 			t.Fail()
 		}
@@ -18,7 +18,7 @@ func TestDo(t *testing.T) {
 	t.Run("Should assign available variables when lesser source", func(t *testing.T) {
 		names := []string{"Tom", "Jerry"}
 		var tom, jerry string
-		Do(names, &tom, &jerry)
+		_ = Do(names, &tom, &jerry)
 		if tom != "Tom" && jerry != "Jerry" {
 			t.Fail()
 		}
@@ -27,7 +27,7 @@ func TestDo(t *testing.T) {
 	t.Run("Should assign available variables when lesser destination", func(t *testing.T) {
 		names := []string{"Tom", "Jerry", "Sparrow"}
 		var tom, jerry string
-		Do(names, &tom, &jerry)
+		_ = Do(names, &tom, &jerry)
 		if tom != "Tom" && jerry != "Jerry" {
 			t.Fail()
 		}
@@ -40,7 +40,7 @@ func TestDo(t *testing.T) {
 		}
 		users := []User{{Name: "Tom", Age: 30}, {Name: "Jerry", Age: 40}}
 		var tom, jerry User
-		Do(users, &tom, &jerry)
+		_ = Do(users, &tom, &jerry)
 		if tom.Name != "Tom" && tom.Age != 30 && jerry.Name != "Jerry" && jerry.Age != 40 {
 			t.Fail()
 		}
@@ -53,7 +53,7 @@ func TestDo(t *testing.T) {
 		}
 		users := [2]User{{Name: "Tom", Age: 30}, {Name: "Jerry", Age: 40}}
 		var tom, jerry User
-		Do(users, &tom, &jerry)
+		_ = Do(users, &tom, &jerry)
 		if tom.Name != "Tom" && tom.Age != 30 && jerry.Name != "Jerry" && jerry.Age != 40 {
 			t.Fail()
 		}
@@ -66,7 +66,7 @@ func TestDo(t *testing.T) {
 		}
 		var names interface{}
 		var tom, jerry User
-		Do(names, &tom, &jerry)
+		_ = Do(names, &tom, &jerry)
 		if tom.Name != "" && tom.Age != 0 && jerry.Name != "" && jerry.Age != 0 {
 			t.Fail()
 		}
@@ -78,6 +78,29 @@ func TestDo(t *testing.T) {
 			Age  int
 		}
 		users := []User{{Name: "Tom", Age: 30}, {Name: "Jerry", Age: 40}}
-		Do(users)
+		_ = Do(users)
+	})
+
+	t.Run("Should return error while empty destination", func(t *testing.T) {
+		names := []string{"Tom", "Jerry"}
+		if err := Do(names); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("Should return error while source and destination type mismatch", func(t *testing.T) {
+		names := []string{"Tom", "Jerry"}
+		var tom int
+		if err := Do(names, &tom); err == nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("Should return error while the destination is not a pointer", func(t *testing.T) {
+		names := []string{"Tom", "Jerry"}
+		var tom string
+		if err := Do(names, tom); err == nil {
+			t.Fail()
+		}
 	})
 }
